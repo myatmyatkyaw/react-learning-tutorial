620be1788ee257620ad84cc2247153553b500553
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-01-18 15:31:47 +0630

    reactbasic

---
+++ src/Block1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './block1.css'
+
+export default function Block1() {
+  return (
+    <>
+    <div className='block1'>
+      <p className='text-primary'>Hello block1 pr</p>
+    </div>
+    </>
+  )
+}
---
+++ src/Block2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './block2.css'
+
+export default function Block2() {
+  return (
+    <>
+    <div className='block2'>
+      <p>Hello block2 pr</p>
+    </div>
+    </>
+  )
+}
