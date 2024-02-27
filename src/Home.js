9654a292d25239e5b1cdbc5303f5077a933d4e07
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-02-27 10:11:36 +0630

    daythree

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const { error, isPending, data: blogs } = useFetch('https://json-server-brown-two.vercel.app/')
 
   return (
     <div className="home">
